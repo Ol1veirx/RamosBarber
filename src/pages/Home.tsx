b2427fd301878c80939dcee92410886b546215a9
Liedson <liedsonwork@hotmail.com>
2025-02-18 11:42:42 -0300

    feat: erro de build

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
 import Header from '../components/Header/Header';
-import { MapPin, Clock, Scissors } from 'lucide-react';
+import { MapPin, Clock } from 'lucide-react';
 import { Card, CardContent } from '../components/Card/Card';
 import './Home.css';
 
